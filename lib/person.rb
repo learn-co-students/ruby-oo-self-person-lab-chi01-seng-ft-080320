126c21207548abdd8534643073fc10fef41ef1da
Ramazan774 <ramazan.ramazanov@glion.ch>
2020-08-05 11:22:16 -0500

    message

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,26 @@
 # your code goes here
+class Person
+
+attr_reader :name, :hygiene
+attr_accessor :bank_account, :happiness 
+
+def initialize(name)
+@name = name
+@bank_account = 25
+@happiness = 8
+@hygiene = 8
+end
+
+# def happiness= (happiness)
+# if happiness == (0..10)
+# @happiness = happiness
+# end 
+# end
+
+def hygiene= (hygiene)
+if hygiene (0..10)
+@hygiene = hygiene
+end
+end
+
+end
