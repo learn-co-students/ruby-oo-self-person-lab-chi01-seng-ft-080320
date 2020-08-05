5309f705aedf95f91e815380cad7d834280e9d31
jjenkins120 <jjenkins120@gmail.com>
2020-08-05 12:04:09 -0500

    2nd commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,28 +1,46 @@
+require 'pry'
+
 class Person
 
-    attr_reader :name, :happiness
+    attr_reader :name, :happiness, :hygiene
     attr_accessor :bank_account
 
-def initialize(name= name, happiness = 8)
-    @name = name
-    @bank_account = 25
-    @happiness = happiness
-end
-
-def name 
-    @name
-end
-
-def happiness=(value)
-    if value > 10 
-        @happiness = 10
-    elsif value < 0 
-        @happiness = 0
-    else @happiness = happiness
+    def initialize(name = name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
     end
+
+    def name 
+        @name
+    end
+
+    def happiness=(value)
+        if value > 10 
+            @happiness = 10
+        elsif value < 0 
+            @happiness = 0
+        else 
+            @happiness = happiness
+        end
+    end
+
+    def hygiene=(value)
+        if value > 10 
+            @hygiene = 10
+        elsif value < 0 
+            @hygiene = 0
+        else 
+            @hygiene = hygiene
+        end
+    end
+
+    def happy?
+        happiness > 7
+    end   
+
 end
-
-
-end#
 
 Steven = Person.new
+Steven.happy?
