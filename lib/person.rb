8d376ee2762bcd2cd79067adee279ffbb9a9287f
gigglesluvscereal <dsg2386@gmail.com>
2020-08-05 11:09:24 -0500

    1st commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,28 @@
-# your code goes here
+class Person
+
+    attr_reader :name, :happiness
+    attr_accessor :bank_account
+
+def initialize(name= name, happiness = 8)
+    @name = name
+    @bank_account = 25
+    @happiness = happiness
+end
+
+def name 
+    @name
+end
+
+def happiness=(value)
+    if value > 10 
+        @happiness = 10
+    elsif value < 0 
+        @happiness = 0
+    else @happiness = happiness
+    end
+end
+
+
+end#
+
+Steven = Person.new
