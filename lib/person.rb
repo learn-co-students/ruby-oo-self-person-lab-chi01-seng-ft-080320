ff0de122578c6fb7e8732ccd8ab773798537d87d
Brian Butterly <butterlybrian@gmail.com>
2020-08-05 12:06:46 -0500

    first_draft

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,20 @@
-# your code goes here
+require 'pry'
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+      @name = name.freeze
+      @bank_account  = 25
+      @happiness = 8 
+      @hygiene = 8  
+    end
+
+    def remember_name
+        @name 
+    end
+
+    def limit
+       @happiness.clamp(0..10)
+    end
+end
