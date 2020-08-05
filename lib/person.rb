b99e4b6bca282e0be5df21014587f487791f8c22
KRStoddard <kimberlynrstoddard@gmail.com>
2020-08-05 11:51:58 -0500

    almost there

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,9 @@
 # your code goes here
 class Person
 
-attr_reader :name, :hygiene
-attr_accessor :bank_account, :happiness 
+attr_reader :name
+attr_writer :happiness, :hygiene 
+attr_accessor :bank_account 
 
 def initialize(name)
 @name = name
@@ -11,16 +12,43 @@ def initialize(name)
 @hygiene = 8
 end
 
-# def happiness= (happiness)
-# if happiness == (0..10)
-# @happiness = happiness
-# end 
-# end
+def happiness
+    @happiness.clamp(0,10)
+end 
 
-def hygiene= (hygiene)
-if hygiene (0..10)
-@hygiene = hygiene
-end
+def hygiene
+    @hygiene.clamp(0,10)
 end
 
+def happy?
+    if @happiness > 7
+        true
+    else
+        false
+    end 
+end 
+
+def clean?
+    if @hygiene > 7
+        true
+    else
+        false
+    end
+end  
+
+def get_paid(salary)
+    @bank_account += salary
+    p "all about the benjamins"
+end
+
+def take_bath(hygiene_points = 4)
+    self.hygiene += hygiene_points
+    p "♪ Rub-a-dub just relaxing in the tub ♫"
+end
+
+def work_out(happiness_points=2, hygiene_points=3)
+    self.happiness += happiness_points
+    self.hygiene -= hygiene_points
+    p "♪ another one bites the dust ♫"
+end
 end
